4bc905e870256e618f383ccdff09c53dd68814f4
Connor Horman <chorman64@gmail.com>
2018-12-05 17:53:19 -0500

    Upload Launcher

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author chorm
+ *
+ */
+module github.chorman0773.sentry.launcher {
+	requires transitive github.chorman0773.sentry;
+	requires github.chorman0773.sentry.mod;
+}
